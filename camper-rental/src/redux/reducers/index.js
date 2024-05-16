c61a758fe4b2dbda6b08344d04e80cb0800b5f02
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-05-16 20:38:05 +0300

    part-1

---
+++ camper-rental/src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import advertsReducer from "./advertsReducer";
+
+const rootReducer = combineReducers({
+  adverts: advertsReducer,
+});
+
+export default rootReducer;
